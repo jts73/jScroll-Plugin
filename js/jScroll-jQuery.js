2f41289054459fd97f4e78f1ea4590c994e5694c
Justin Schneider <justin.schneider91@gmail.com>
2013-06-16 11:38:44 -0600

    jScroll Plugin Structure

--- js/jScroll-jQuery.js
+++ js/jScroll-jQuery.js
@@ -1,7 +1,50 @@
+// Utility
+if (Object.create != 'function') {
+	Object.create = function( obj ) {
+		function jScroll() {};
+		jScroll.prototype = obj;
+
+		return new jScroll();
+	}
+}
+
 (function($, window, document, undefined) {
 
-	$.fn.jScroll = function() {
-		
+	var obj = {
+		init: function(options, elem) {
+			this.elem = elem;
+			this.$elem = $(elem);
+			this.$win = $(window);
+			this.elemTop = parseInt(this.$elem.css('margin-top'));
+
+			this.settings = $.extend({}, $.jScroll.defaults, options);
+
+			this._default();
+		},
+
+		_default: function() {
+			this.$win.on('scroll', function() {
+				console.log('scrolling');
+			});
+		}
+	};
+
+	$.fn.jScroll = function( options ) {
+
+		return this.each(function() {
+			var jScroll = Object.create(obj);
+
+			jScroll.init(options, this);
+		});
+
+	}
+
+	$.jScroll = {
+		defaults: {
+			easing: 'swing',
+			speed: 600,
+			margin: 50
+		}
 	}
 	
 })(jQuery, window, document);
